7061852432f7a3d41029bbca3377457b5c7f5514
Iago Bruno <httpiago@gmail.com>
2018-05-10 13:29:17 -0300

    Nova sessão "sobre" na página inicial e algumas correções no CSS

--- assets/functions.js
+++ assets/functions.js
@@ -2,9 +2,10 @@
 
 // Mudar a altura do cabeçalho ao carregar a página e quando a janela for redimensionada.
 window.onresize = setHeaderHeight = function () {
-  var headerPadding = (32 * 2) + 1,
+  var headerPadding = ((50) * 2) + 1,
     minHeight = 360,
-    height = Math.max(minHeight, (window.innerHeight - headerPadding));
+    maxHeight = 580,
+    height = Math.min(Math.max(minHeight, (window.innerHeight - headerPadding)), maxHeight);
 
   document.getElementsByClassName('header-container')[0].style.height = height + 'px';
 };
