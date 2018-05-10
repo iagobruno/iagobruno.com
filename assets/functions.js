be1609e6c3b23734bd8754e176ac127dcdeb8667
Iago Bruno <httpiago@gmail.com>
2018-05-10 18:58:08 -0300

    Correções e melhorias

--- assets/functions.js
+++ assets/functions.js
@@ -2,9 +2,9 @@
 
 // Mudar a altura do cabeçalho ao carregar a página e quando a janela for redimensionada.
 window.onresize = setHeaderHeight = function () {
-  var headerPadding = ((50) * 2) + 1,
+  var headerPadding = ((50) * 2),
     minHeight = 360,
-    maxHeight = 580,
+    maxHeight = 540,
     height = Math.min(Math.max(minHeight, (window.innerHeight - headerPadding)), maxHeight);
 
   document.getElementsByClassName('header-container')[0].style.height = height + 'px';
