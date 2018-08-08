9ef2ba51b3fd32172512e82086e22544335d18bc
Iago Bruno <httpiago@gmail.com>
2018-08-08 18:45:19 -0300

    Correção de um erro de "escape" de código do GA

--- components/GA.js
+++ components/GA.js
@@ -3,12 +3,12 @@ import React, { Fragment } from 'react'
 export default () => (
   <Fragment>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119109259-1"></script>
-    <script>
+    <script dangerouslySetInnerHTML={{__html: `
       window.dataLayer = window.dataLayer || []; 
       function gtag() { dataLayer.push(arguments); } 
       gtag('js', new Date()); 
  
       gtag('config', 'UA-119109259-1');
-    </script>
+    `.replace(/\r?\n|\r/g, '')}}></script>
   </Fragment>
 )
