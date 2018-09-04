e9b34b400ce8cbf98e721629928851e4f3b27ce8
Iago Bruno <httpiago@gmail.com>
2018-09-04 16:40:38 -0300

    Update About.js

--- components/About.js
+++ components/About.js
@@ -72,7 +72,7 @@ export default class About extends Component {
           <div>
             <p><b>Hello, World!</b></p>
             <p>Me chamo Iago, moro no Ceará, tenho 22 anos de idade e sou programador desde os 15. Sou apaixonado por códigos e aprendi tudo sozinho, movido pela curiosidade de saber como a web funcionava, desde então, venho estudando novas linguagens programação, conceituando e desenvolvendo websites, como o design do <a href="#work-tecbolt">TecBolt</a>, que construí em parceria com o fundador do mesmo.</p>
-            <p>Possuo experiência vasta em HTML, CSS, JavaScript, PHP e estando ainda a aprender a dominar o desenvolvimento de aplicativos para celulares. <br/>Você pode conferir <a href="#skills">minhas outras habilidades</a> abaixo.</p>
+            <p>Possuo experiência vasta em HTML, CSS, JavaScript, PHP e estando ainda a aprender a dominar o desenvolvimento de aplicativos para celulares utilizando o React Native. <br/> Você pode conferir <a href="#skills">minhas outras habilidades</a> abaixo.</p>
           </div>
         </center>
       </section>
