63fbe0b13098c767ba4886a5b46f23597e6db3af
Iago Bruno <httpiago@gmail.com>
2019-02-18 03:18:31 -0300

    Correções e uso dos react hooks

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import Delegate from 'delegate'
 
 import '../styles/main.less'
 
@@ -15,6 +16,9 @@ export default class MyApp extends App {
   }
   
   componentDidMount() {
+    // Delegar um evento global para todos os links da página
+    Delegate('a', 'click', this.handleLinkClick)
+    
     // Ativar o modo design durante o desenvolvimento
     if (process.env.NODE_ENV === 'development') {
       document.designMode = 'on'
@@ -22,6 +26,28 @@ export default class MyApp extends App {
       let logStyle = 'background: green; border-radius:2px; color:white; padding: 0 4px;'
       console.log('%cDesign mode: ON', logStyle, 'Você pode editar qualquer texto diretamente no DOM.')
     }
+  }
+  
+  handleLinkClick(event) {
+    let link = event.delegateTarget,
+      url = link.href
+    
+    // Checar se é um link específico da sessão "Sobre"
+    if (/\#work-tecbolt$/g.test(url)) {
+      event.preventDefault()
+  
+      // Mover o scroll até a sessão #works
+      location.hash = '#works'
+  
+      // Dar foco ao link do tecbolt
+      document.getElementById('work-tecbolt').focus()
+    }
+
+    // Enviar evento para o Google Analytics
+    gtag('event', 'click', {
+      'event_category': 'links',
+      'event_label': 'Cliques em "' + url + '"'
+    })
   }
 
   render () {
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import Head from 'next/head'
 
 import Header from '../components/Header'
@@ -8,55 +8,21 @@ import Skills from '../components/Skills'
 import Works from '../components/Works'
 import Footer from '../components/Footer'
 
-const Delegate = require('delegate')
+export default function HomePage() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Iago Bruno &#8212; Full Stack Developer</title>
+      </Head>
 
-class HomePage extends Component {
-  componentDidMount() {
-
-    // Delegar um evento global para todos os links da página
-    Delegate('a', 'click', this.linkClick)
-  }
-
-  linkClick(event) {
-    let link = event.delegateTarget,
-      url = link.href
-    
-    // Checar se é um link específico da sessão "Sobre"
-    if (/\#work-tecbolt$/g.test(url)) {
-      event.preventDefault()
-  
-      // Mover o scroll até a sessão #works
-      location.hash = '#works'
-  
-      // Dar foco ao link do tecbolt
-      document.getElementById('work-tecbolt').focus()
-    }
-
-    // Enviar evento para o Google Analytics
-    gtag('event', 'click', {
-      'event_category': 'links',
-      'event_label': 'Cliques em "' + url + '"'
-    })
-  }  
-
-  render() {
-    return (
-      <Fragment>
-        <Head>
-          <title>Iago Bruno &#8212; Full Stack Developer</title>
-        </Head>
-
-        <div className="home-page">
-          <Header />
-          <About />
-          <CreativeProcess />
-          <Skills />
-          <Works />
-          <Footer />
-        </div>
-      </Fragment>
-    )
-  }
+      <div className="home-page">
+        <Header />
+        <About />
+        <CreativeProcess />
+        <Skills />
+        <Works />
+        <Footer />
+      </div>
+    </Fragment>
+  )
 }
-
-export default HomePage
